a48d5d12079318d3b44cef04b7c36955dd390d1d
vannhgch211217 <vannhgch211217@fpt.edu.vn>
2022-12-19 21:15:47 +0700

    crud admin lại

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -19,7 +19,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Psr\Log\LoggerInterface;
-
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * @Route("/product")
@@ -50,6 +50,69 @@ class ProductController extends AbstractController
         [
             'product' => $product
         ]
+        );
+    }
+    #[Route('/crud', name: 'crud_product_index', methods: ['GET'])]
+    public function index(EntityManagerInterface $em): Response
+    {
+        $product= $em->getRepository(Product::class)->findAll();
+        return $this->render('crud_product/index.html.twig', [
+            'list' => $product,
+        ]);
+    }
+
+    #[Route('/crud/create', name: 'crud_product_create')]
+    public function create(Request $request, ManagerRegistry $doctrine):Response
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //gọi đến Manager (Doctrine) để add object (row in table)
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Product created successfully');
+            return $this->redirectToRoute('crud_product_index');
+        }
+         
+        return $this->render('crud_product/create.html.twig', [
+            'form' => $form -> createView(),
+        ]);
+    }
+
+    #[Route('/crud/update/{id}', name: 'crud_product_update')]
+    public function update(Request $request, ManagerRegistry $doctrine,$id, EntityManagerInterface $em):Response
+    {   
+        $product= $em->getRepository(Product::class)->find($id);
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //gọi đến Manager (Doctrine) để add object (row in table)
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Product updated successfully');
+            return $this->redirectToRoute('crud_product_index');
+        }
+         
+        return $this->render('crud_product/update.html.twig', [
+            'form' => $form -> createView(),
+        ]);
+    }
+    #[Route('/crud/delete/{id}', name: 'crud_product_delete')]
+    public function delete(Request $request, ManagerRegistry $doctrine,$id, EntityManagerInterface $em):Response
+    {   
+        $product= $em->getRepository(Product::class)->find($id);
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($product,true);
+        $entityManager->flush();
+         
+        return $this->redirectToRoute('crud_product_index' 
         );
     }
     
