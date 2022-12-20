1358f8c488f47c69c9a62f2b97893590a156ca22
Nguyen Duy Lam Tung <tungndlgch210001@fpt.edu.vn>
2022-12-20 09:33:36 +0700

    SQL update

--- ShoppingCart.sql
+++ ShoppingCart.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Cơ sở dữ liệu: `shoppingcart`
 --
-
+CREATE DATABASE IF NOT EXISTS `ShoppingCart` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `ShoppingCart`;
 -- --------------------------------------------------------
 
 --
