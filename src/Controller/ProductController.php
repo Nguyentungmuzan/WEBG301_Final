adb1d76146e0be690994872869d34e8d2c8926f0
vannhgch211217 <vannhgch211217@fpt.edu.vn>
2022-12-20 15:08:58 +0700

    úp ảnh

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,23 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Product;
-use App\Entity\Order;
-use App\Entity\OrderDetail;
 use App\Form\ProductType;
-use App\Repository\OrderRepository;
-use App\Repository\OrderDetailRepository;
-use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
-use Doctrine\ORM\Tools\Pagination\Paginator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Psr\Log\LoggerInterface;
 use Doctrine\ORM\EntityManagerInterface;
+
 
 /**
  * @Route("/product")
@@ -27,15 +20,15 @@ use Doctrine\ORM\EntityManagerInterface;
 class ProductController extends AbstractController
 {
     #[Route('/', name: 'product_index')]
-    public function viewtestw(ProductRepository $productRepository, Request $request)
+    public function viewtestw(ProductRepository $productRepository)
     {
-        $searchValue = $request->get('searchValue') ? $request->get('searchValue') :'';
-        $products = $productRepository->searchProducts($searchValue);
-        return $this->render('product/index.html.twig', 
-        [
-            'products' => $products
-        ]
-    );
+        $products = $productRepository->findAll();
+        return $this->render(
+            'product/index.html.twig',
+            [
+                'products' => $products
+            ]
+        );
     }
 
     #[Route('/detail/{id}', name: 'product_show')]
@@ -43,27 +36,27 @@ class ProductController extends AbstractController
     {
         $product = $productRepository->find($id);
         if ($product == null) {
-            $this ->addFlash('Error', 'Product not found');
+            $this->addFlash('Error', 'Product not found');
             return $this->redirectToRoute('product_index');
         }
         return $this->render(
-            'product/show.html.twig', 
-        [
-            'product' => $product
-        ]
+            'product/show.html.twig',
+            [
+                'product' => $product
+            ]
         );
     }
     #[Route('/crud', name: 'crud_product_index', methods: ['GET'])]
     public function index(EntityManagerInterface $em): Response
     {
-        $product= $em->getRepository(Product::class)->findAll();
+        $product = $em->getRepository(Product::class)->findAll();
         return $this->render('crud_product/index.html.twig', [
             'list' => $product,
         ]);
     }
 
     #[Route('/crud/create', name: 'crud_product_create')]
-    public function create(Request $request, ManagerRegistry $doctrine):Response
+    public function create(Request $request, ManagerRegistry $doctrine): Response
     {
         $product = new Product();
         $form = $this->createForm(ProductType::class, $product);
@@ -84,7 +77,7 @@ class ProductController extends AbstractController
             //B5: di chuyển ảnh về thư mục chỉ định trong project
             try {
                 $file->move(
-                    $this->getParameter('book_image'),
+                    $this->getParameter('product_imgurl'),
                     $imageName
                     //di chuyển file ảnh upload về thư mục cùng với tên mới
                     //note: cầu hình parameter trong file services.yaml
@@ -102,20 +95,39 @@ class ProductController extends AbstractController
             $this->addFlash('Info', 'Add book successfully !');
             return $this->redirectToRoute('crud_product_index');
         }
-         
+
         return $this->render('crud_product/create.html.twig', [
-            'form' => $form -> createView(),
+            'form' => $form->createView(),
         ]);
     }
 
     #[Route('/crud/update/{id}', name: 'crud_product_update')]
-    public function update(Request $request, ManagerRegistry $doctrine,$id, EntityManagerInterface $em):Response
-    {   
-        $product= $em->getRepository(Product::class)->find($id);
+    public function update(Request $request, ManagerRegistry $doctrine, $id, EntityManagerInterface $em): Response
+    {
+        $product = $em->getRepository(Product::class)->find($id);
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            //kiểm tra xem người dùng có muốn upload ảnh mới hay không
+            //nếu có thì thực hiện code upload ảnh
+            //nếu không thì bỏ qua
+            $imageFile = $form['imgurl']->getData();
+            if ($imageFile != null) {
+                $file = $form->get('imgurl')->getData();
+                $imgName = uniqid(); 
+                $imgExtension = $file->guessExtension();
+               $imageName = $imgName . "." . $imgExtension;
+               try {
+                  $file->move(
+                     $this->getParameter('product_imgurl'),
+                     $imageName
+                  );
+               } catch (FileException $e) {
+                  throwException($e);
+               }
+               $product->setImgurl($imageName);
+            }
             //gọi đến Manager (Doctrine) để add object (row in table)
             $entityManager = $doctrine->getManager();
             $entityManager->persist($product);
@@ -124,22 +136,23 @@ class ProductController extends AbstractController
             $this->addFlash('Info', 'Update book successfully !');
             return $this->redirectToRoute('crud_product_index');
         }
-         
+
         return $this->render('crud_product/update.html.twig', [
-            'form' => $form -> createView(),
+            'form' => $form->createView(),
         ]);
     }
     #[Route('/crud/delete/{id}', name: 'crud_product_delete')]
-    public function delete(Request $request, ManagerRegistry $doctrine,$id, EntityManagerInterface $em):Response
-    {   
-        $product= $em->getRepository(Product::class)->find($id);
+    public function delete(Request $request, ManagerRegistry $doctrine, $id, EntityManagerInterface $em): Response
+    {
+        $product = $em->getRepository(Product::class)->find($id);
         $entityManager = $doctrine->getManager();
-        $entityManager->remove($product,true);
+        $entityManager->remove($product, true);
         $entityManager->flush();
         $this->addFlash('Info', 'Delete book successfully !');
-         
-        return $this->redirectToRoute('crud_product_index' 
+
+        return $this->redirectToRoute(
+            'crud_product_index'
         );
     }
-    
+
 }
